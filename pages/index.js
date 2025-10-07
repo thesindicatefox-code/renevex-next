724914c60f6dcb19300374920c4d55a9fdaca9e5
thesindicatefox-code <thesindicatefox@gmail.com>
2025-10-07 13:17:23 -0500

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import Features from "@/components/Features";
-import CTA from "@/components/CTA";
-import Footer from "@/components/Footer";
-
-export default function Home(){return(<div className="min-h-screen bg-brand text-white"><Navbar/><main className="container"><Hero/><Features/><CTA/></main><Footer/></div>)}
